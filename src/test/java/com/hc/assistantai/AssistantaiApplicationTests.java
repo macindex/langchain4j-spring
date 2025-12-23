917b93a6edf13e2824616164cb4959fc4fd1f131
macindex <machollanda@gmail.com>
2025-12-23 15:30:59 -0300

    first commit

---
+++ src/test/java/com/hc/assistantai/AssistantaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hc.assistantai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistantaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
